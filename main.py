55bb56bbcc9d0e02f5ee14474ca12b68793b4a83
Batuhan GÜRBÜZ <batuhang002@gmail.com>
2025-05-03 12:44:41 +0300

    Project finish

---
+++ main.py
@@ -0,0 +1,50 @@
+import cv2
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
+
+# Functions 
+
+def openFile():
+    filePath = filedialog.askopenfilename()
+    
+    if filePath:
+        img = cv2.imread(filePath)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.30, minNeighbors=5, minSize=(30,30))
+
+        for(x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2)
+            cv2.putText(img, "insan", (x,y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
+            
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img)
+        img = img.resize((600,400), Image.LANCZOS)
+        img = ImageTk.PhotoImage(img)
+        
+        canvas.img = img
+        canvas.create_image(0,0, anchor = tk.NW, image = img)
+        
+
+faceCascade = cv2.CascadeClassifier('face_detector.xml')
+        
+        
+# Design
+root = tk.Tk()
+root.title("Yüz Tanıma")
+canvas = tk.Canvas(root, width=600, height=400)
+canvas.pack()
+button = tk.Button(root, text="Dosya Seç", command=openFile)
+button.pack()
+
+
+
+
+
+
+
+
+
+root.mainloop()
